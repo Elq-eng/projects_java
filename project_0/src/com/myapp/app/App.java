956d84ec9cf82f9fa980de400ce825d71c445a64
elq-eng <ecascavita17@unisalle.edu.co>
2025-07-16 21:46:14 -0500

    project0.5

---
+++ project_0/src/com/myapp/app/App.java
@@ -0,0 +1,12 @@
+
+package com.myapp.app;
+
+import com.myapp.app.utils.Utilidad;
+
+
+public class App{
+    public static void main( String[] args ){
+        System.out.println("Inicio del programa");
+        Utilidad.saludar();
+    }
+}
---
+++ project_0/src/com/myapp/app/utils/Utilidad.java
@@ -0,0 +1,10 @@
+
+
+
+package com.myapp.app.utils;
+
+public class Utilidad {
+    public static void saludar(){
+        System.out.println("Hola desde la clase de utilidad");
+    }
+}
