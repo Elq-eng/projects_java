2827cf181b22b9067853b8bc90134b1818602f80
elq-eng <ecascavita17@unisalle.edu.co>
2025-07-21 23:36:58 -0500

    feat:objects

--- project_01/src/main/java/com/project/Main.java
+++ project_01/src/main/java/com/project/Main.java
@@ -7,7 +7,7 @@ import main.java.com.project.bases.operadores_basicos.OperatorBasic; //src/main/
 import main.java.com.project.bases.control_de_flujo.FlowControl; //src/main/java/com/project/bases/operadores_basicos/OperatorBasic.java
 import main.java.com.project.bases.loops.Loops;
 import main.java.com.project.bases.coleccionesBasics.Collections;
-
+import main.java.com.project.bases.clases.Car;
 
 public class Main {
 
@@ -24,6 +24,23 @@ public class Main {
         Loops.main();
 //        colections
         Collections.main();
+//        objects
+        Car car1 = new Car("Toyota", "Corolla", 2020);
+        Car car2 = new Car("Chevrolet", "Onix", 2023);
+
+//        usar metods
+        car1.showInformation();
+        car1.turnOff();
+        car1.turnOn();
+
+        System.out.println();
+
+        car2.showInformation();
+        car2.turnOff();
+        car2.setState(true);
+        car2.turnOn();
+
+
 
     }
 }
---
+++ project_01/src/main/java/com/project/bases/clases/Car.java
@@ -0,0 +1,75 @@
+
+package main.java.project.bases.clases;
+
+public class Car {
+
+//    atributos privados ( encapsulacion )
+    private String mark;
+    private String model;
+    private int year;
+    private boolean state;
+
+//    constructor
+    public Car( String mark, String model, int year){
+        this.mark = mark;
+        this.model = model;
+        this.year = year;
+        this.state = false;
+    }
+
+//Metodos publicos para acceder a los atributos (Getters y Setters)
+    public String getMark(){
+        return mark;
+    }
+
+    public void setMark( String mark){
+        this.mark = mark;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public void turnOff(){
+        if(state){
+            state = false;
+            System.out.println("El carro se ha apagado");
+        }else{
+            System.out.println("El carro ya estaba apagado");
+        }
+    }
+
+    public void turnOn(){
+        if(!state){
+            state = true;
+            System.out.println("El carro se ha encedido");
+        }else{
+            System.out.println("El carro ya estaba encendido");
+        }
+    }
+
+    public void showInformation(){
+        System.out.println("Mark: " + mark + "Model: " + model + "year: " + year);
+    }
+
+}
